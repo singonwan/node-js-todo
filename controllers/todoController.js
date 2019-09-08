df51863aa49b653f6a08f36df6bde77fa217de29
George Wan <singonwan@gmail.com>
2019-09-08 17:20:58 -0400

    added the ajax delete function and it works on the dummy data

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -15,8 +15,11 @@ app.post('/todo', urlencodedParser, function(req, res){
     res.json(data);
 });
 
-app.delete('/todo', function(req, res){
-
+app.delete('/todo/:item', function(req, res){
+    data = data.filter(function(todo){
+        return todo.item.replace(/ /g,'-') !== req.params.item;
+    })
+    res.json(data);
 });
 
 };
