50bcd31d3d4985d0b6aa5bd6face8fbb05905732
George Wan <singonwan@gmail.com>
2019-09-08 20:06:00 -0400

    added mongoose and connected to mongo db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb+srv://gwan123:gwan123@cluster0-da878.mongodb.net/test?', { useNewUrlParser: true });
+
+//create a schema
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'get starbucks'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}]
 
@@ -10,11 +25,13 @@ app.get('/todo', function(req, res){
     res.render('todo', {todos: data});
 });
 
+//AJAX POST handler
 app.post('/todo', urlencodedParser, function(req, res){
     data.push(req.body);
     res.json(data);
 });
 
+//AJAX DELETE handler
 app.delete('/todo/:item', function(req, res){
     data = data.filter(function(todo){
         return todo.item.replace(/ /g,'-') !== req.params.item;
